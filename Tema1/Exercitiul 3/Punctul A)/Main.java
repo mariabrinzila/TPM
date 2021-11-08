7305f8cbb7d6946406e9890e8d2829a8f42d227f
mariabrinzila <58850569+mariabrinzila@users.noreply.github.com>
2021-11-08 22:17:19 +0200

    Update Main.java

--- Tema1/Exercitiul 3/Punctul A)/Main.java
+++ Tema1/Exercitiul 3/Punctul A)/Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Pot p = Pot.getInstance();
         p.setNr_total_portions(4);
         p.setNr_left_portions(4);
